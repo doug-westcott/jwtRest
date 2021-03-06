a625541d22c1a373d72e6619f6af64c716456fc7
user <doug.westcott@gmail.com>
2021-03-06 11:10:30 +0300

    update

--- src/main/java/com/example/demo/repository/CartItemRepository.java
+++ src/main/java/com/example/demo/repository/CartItemRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.model.Cartitem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartItemRepository extends JpaRepository<Cartitem, Long> {
+    List<Cartitem> findByProductid(Long id);
 }
